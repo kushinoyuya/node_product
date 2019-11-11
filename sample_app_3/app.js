d355682ba6873de6390ec873caf0a3789ef021b2
kushinoyuya <kusshy15059855@gmail.com>
2019-11-11 22:55:03 +0900

    app.js_1

--- sample_app_3/app.js
+++ sample_app_3/app.js
@@ -8,11 +8,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 // ログイン画面
-var login = require('./routes/login');
+var loginRouter = require('./routes/login');
 // 新規登録画面
-var register = require('./routes/register');
+var registerRouter = require('./routes/register');
 // 登録完了・ログイン後画面
-var confirm = require('./routes/confirm');
+var confirmRouter = require('./routes/confirm');
 
 var app = express();
 
@@ -26,19 +26,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// トップ画面
 app.use('/', indexRouter);
-app.post('/login', function (req, res, next) {
+app.post('/', indexRouter, function (req, res, next) {
   next('login')
 });
 
 app.use('/users', usersRouter);
+
 // ログイン画面
-app.use('/login', login);
+app.use('/login', loginRouter);
+app.post('/login', loginRouter, function (req, res, next) {
+  next('confirm')
+})
 // 新規登録画面
-app.use('/register', register);
+app.use('/register', registerRouter);
+app.post('/register', registerRouter, function (req, res, next) {
+  next('confirm')
+})
 // 登録完了・ログイン後画面
-app.use('/confirm', confirm);
+app.use('/confirm', confirmRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
